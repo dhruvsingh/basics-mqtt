eff894e4c8fa003383662e80994cb8e8db6a8d0d
Dhruv Singh <dhruvsingh.er@gmail.com>
2018-12-16 19:29:37 +0530

    Subscriber listens to test

--- subscriber.py
+++ subscriber.py
@@ -10,10 +10,13 @@ https://www.ev3dev.org/docs/tutorials/sending-and-receiving-messages-with-mqtt/
 import paho.mqtt.client as mqtt
 
 
+host = 'localhost'
+
+
 def on_connect(client, userdata, flags, rc):
     """On client connect."""
     print("Connected with result code " + str(rc))
-    client.subscribe("topic/test")
+    client.subscribe("/test")
 
 
 def on_message(client, userdata, msg):
@@ -22,10 +25,12 @@ def on_message(client, userdata, msg):
         print("Yes!")
         client.disconnect()
 
-client = mqtt.Client()
-client.connect("THE_IP_ADDRESS_OF_OUR_BROKER", 1883, 60)
 
-client.on_connect = on_connect
-client.on_message = on_message
+if __name__ == '__main__':
+    client = mqtt.Client()
+    client.connect(host, 1883, 60)
 
-client.loop_forever()
+    client.on_connect = on_connect
+    client.on_message = on_message
+
+    client.loop_forever()
